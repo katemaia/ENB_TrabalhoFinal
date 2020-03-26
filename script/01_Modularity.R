e8f8081ccee0dc3b2e80b6a1e87167b519fd4479
katemaia <kate.spirogyra@gmail.com>
2020-03-26 18:26:00 -0300

    plot network: falta microrreg codes

--- script/01_Modularity.R
+++ script/01_Modularity.R
@@ -7,11 +7,38 @@
 library(igraph)
 library(RColorBrewer)
 
+dir("./script")
+source("./script/aux_func_C&Z.R")
+
+
+# Leitura dos dados ---------------------------------------------
 dir("./data")
 
-# Leitura da rede ---------------------------------------------
 network <- read.table("./data/network_MG.txt", header = TRUE, sep = "\t", check.names = FALSE)
 class(network)
 dim(network)
 all(rownames(network) == colnames(network))
+
+coords <- read.table("./data/microreg_coords_MG.csv", header = TRUE, sep = ",", check.names = FALSE)
+
+# Analise de modularidade da rede ---------------------------------------------
+
+graph <- graph_from_adjacency_matrix(as.matrix(network), weighted = T, mode = "directed")
+all(V(graph)$name == rownames(network)) # vertex of graph in the same order as matrix row/col
+
+M <- cluster_optimal(graph, weights = E(graph)$weight)
+
+# Dataframe com resultados de membership
+mreg_data <- data.frame(mreg = V(graph)$name, module = M$membership)
+
+# Analise de c & z das microrregioes ---------------------------------------------
+
+mreg_data$c <- c_function(mreg_data, network)
+
+mreg_data$z <- z_function(mreg_data, network)
+
+# Plot rede rodoviaria de Minas Gerais -------------------------------------------
+
+display.brewer.pal(8, "Dark2")
+display.brewer.pal(8, "Set3")
 
