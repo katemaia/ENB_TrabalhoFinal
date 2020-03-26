513ca7dfb850f3c169943201158ef916ba04a155
katemaia <kate.spirogyra@gmail.com>
2020-03-26 15:30:50 -0300

    script 00 praticamente finalizado: so falta comentar

--- script/00_Create_Network.R
+++ script/00_Create_Network.R
@@ -1,7 +1,10 @@
 
+library(igraph)
+
 dir("./data")
-road_data <- read.table("./data/IBGE_ligacoes_rodoviarias_e_hidroviarias_2016.txt", header = TRUE, sep = "\t", quote = "")
-dim(road_data)
+road_data <- read.table("./data/IBGE_ligacoes_rodoviarias_e_hidroviarias_2016.txt", header = TRUE, sep = "\t", quote = "")[, c(3,5,7,9,15)]
+colnames(road_data)[5] <- "fluxo"
+head(road_data)
 
 territ <- read.table("./data/divisao_territorial_brasil.csv", header = TRUE, sep = ";", quote = "")
 head(territ)
@@ -22,11 +25,23 @@ territ$microrreg <- factor(territ$microrreg)
 road_data$microrreg_A <- territ[match(road_data$NOMEMUN_A, territ$municip), "microrreg"]
 road_data$microrreg_B <- territ[match(road_data$NOMEMUN_B, territ$municip), "microrreg"]
 
-road_data[which(is.na(road_data$microrreg_A)),c(5,9)] 
-road_data[which(is.na(road_data$microrreg_B)),c(5,9)]
+road_data[which(is.na(road_data$microrreg_A)),] 
+road_data[which(is.na(road_data$microrreg_B)),]
 territ[592,]
 
 road_data[which(is.na(road_data$microrreg_A)), "microrreg_A"] <- "Caratinga"
 road_data[which(is.na(road_data$microrreg_B)), "microrreg_B"] <- "Caratinga"
 
-road_data[road_data$NOMEMUN_A == "Pingo-d'Água" | road_data$NOMEMUN_B == "Pingo-d'Água", c("microrreg_A", "microrreg_B")]
+road_data[road_data$NOMEMUN_A == "Pingo-d'Água" | road_data$NOMEMUN_B == "Pingo-d'Água",]
+
+# Criando rede de rotas rodoviarias entre microrregioes
+edge_list <- data.frame(from = road_data$microrreg_A, to = road_data$microrreg_B, weight = road_data$fluxo)
+
+edge_list <- aggregate(weight ~ from+to, edge_list, sum)
+
+graph <- graph.data.frame(edge_list, directed = TRUE)
+
+adj_matrix <- get.adjacency(graph, attr = "weight", sparse = FALSE)
+
+
+
